21e646159275b8cf59507bff3d8c5b073f3f4d38
huuquan2898-netizen <huuquan2898@gmail.com>
2026-06-18 23:30:48 +0700

    okeee

--- Assets/Khoa/Menu/Scripts/MainMenu.cs
+++ Assets/Khoa/Menu/Scripts/MainMenu.cs
@@ -1,21 +1,43 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Scene")]
+    [Tooltip("Tên scene map đầu tiên. Nên dùng tên scene thay vì số index để tránh sai Build Settings.")]
+    public string firstMapSceneName = "Map1";
+
     public void Play()
     {
-        SceneManager.LoadScene(1);
+        if (!string.IsNullOrEmpty(firstMapSceneName))
+        {
+            SceneManager.LoadScene(firstMapSceneName);
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
-    // Update is called once per frame
     public void Setting()
     {
-        
+        Debug.Log("MainMenu: Bấm nút Tùy Chọn.");
     }
+
     public void Out()
     {
+        Debug.Log("MainMenu: Bấm nút Thoát Game.");
+
+#if UNITY_EDITOR
+        // Khi đang test trong Unity Editor thì dừng Play Mode.
+        EditorApplication.isPlaying = false;
+#else
+        // Khi build game thật thì thoát game.
         Application.Quit();
+#endif
     }
 }
